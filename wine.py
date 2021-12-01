c061989338cf9938f3a694394751dd4b53cbcd61
Philfeiran <51011052+Philfeiran@users.noreply.github.com>
2021-12-01 14:36:32 -0500

    Add files via upload

--- wine.py
+++ wine.py
@@ -4,7 +4,7 @@ from pyspark.mllib.evaluation import MulticlassMetrics
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.shell import sc
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, FloatType, StringType, IntegerType
+from pyspark.sql.types import StructType, StructField, FloatType, ArrayType, IntegerType,DataType,StringType
 
 
 def main():
@@ -18,6 +18,11 @@ def main():
           .option('header', 'true')
           .load("TrainingDataset.csv", inferSchema='true',header = True ,sep =";"))
 
+
+    testdataframe = (spark.read
+                 .format("csv")
+                 .option('header', 'true')
+                 .load("ValidationDataset.csv", inferSchema='true', header=True, sep=";"))
     data=[]
 
     for row in dataframe.rdd.collect():
@@ -30,25 +35,30 @@ def main():
         data.append(LabeledPoint(label,feature))
     dataset=sc.parallelize(data)
     model = LogisticRegressionWithLBFGS.train(dataset,numClasses=10)
-
-
+    ##dataframe.show(1)
+   ## testdataframe.show(1)
     data2=[]
-    for lp in data:
-        temp="["
-        for i in lp.features:
-            temp+=" "+str(i)+","
-        temp+="]"
-        data2.append((temp,lp.label,model.predict(lp.features)))
+    for row in testdataframe.rdd.collect():
+        feature = [row['"""fixed acidity""""'], row['""""volatile acidity""""'],
+                   row['""""citric acid""""'], row['""""residual sugar""""'],
+                   row['""""chlorides""""'], row['""""free sulfur dioxide""""'],
+                   row['""""total sulfur dioxide""""'], row['""""density""""'] ,
+                   row['""""pH""""'], row['""""sulphates""""'], row['""""alcohol""""']]
+        label = row['""""quality"""""']
+        data2.append([str(feature),label,model.predict(feature)])
 
     schema = StructType([
         StructField("features", StringType(), True), \
-        StructField("label", FloatType(), True), \
+        StructField("label", IntegerType(), True), \
         StructField("prediction", IntegerType(), True) \
         ])
 
     df2 = spark.createDataFrame(data=data2, schema=schema)
     df2.show(20)
-    predictionAndLabel = dataset.map(lambda lp: (float(model.predict(lp.features)), lp.label))
+
+
+    dataset2 = sc.parallelize(data2)
+    predictionAndLabel = dataset2.map(lambda row: (float(row[2]), float(row[1])))
     metrics = MulticlassMetrics(predictionAndLabel)
     f1Score = metrics.weightedFMeasure()
     print("F1 Score = %s" % f1Score)
